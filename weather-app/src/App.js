23006a02b7d499c2f8644367057f340b0846deb8
Taylor Hartsfield <taylormhartsfield@gmail.com>
2022-10-02 14:43:01 -0600

    base app made. Non responsive

--- weather-app/src/App.js
+++ weather-app/src/App.js
@@ -1,10 +1,17 @@
-import './App.css';
+import './css/App.css';
+import Search from './components/Search';
+import Location from './components/Location';
+import CurrentWeather from './components/CurrentWeather';
+
 
 function App() {
   return (
-    <div className="App">
-      Hello!
-      
+    <div className="App cold">
+      <main>
+        <Search />
+        <Location/>
+        <CurrentWeather />
+      </main>
     </div>
   );
 }
